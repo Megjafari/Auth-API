7734f32f2faf792bda09a90fd95c0109a74164b8
Meghdad Jafari <megberserker@gmail.com>
2025-12-16 13:39:11 +0100

    Add AppDbContext and UserRepository with basic DB setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Auth_API.Models;
+using Microsoft.EntityFrameworkCore;// Importing Entity Framework Core
+
+namespace Auth_API.Data
+{
+    public class AppDbContext : DbContext // Application database context
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } // Constructor with options
+
+        public DbSet<User> Users { get; set; } // DbSet for User entities
+
+    }
+}
